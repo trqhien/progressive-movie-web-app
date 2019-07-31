09c3482f053f4722aea212a60620f8f8b298488f
Hien Tran <hien.trquang@gmail.com>
2019-07-31 15:15:30 +0700

    Fix ESLint warnings

--- api.js
+++ api.js
@@ -5,21 +5,16 @@ const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
 const API_KEY = 'dc252f7444d39f39197952cf36f30ee4';
 
 function getMovieById(id) {
-  const url = new URL(`${BASE_URL}/movie/${id}`)
+  const url = new URL(`${BASE_URL}/movie/${id}`);
 
   const params = {
     api_key: API_KEY,
-    language: 'en-US'
-  }
+    language: 'en-US',
+  };
 
-  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
-  fetch(url)
-    .then(asJSON)
-    .then(updateUI)
-    .catch((err) => {
-      console.log(err);
-    });
+  return fetch(url);
 }
 
 function asJSON(response) {
@@ -55,12 +50,21 @@ function updateUI(movie) {
 
   const picture = document.getElementById('poster-image');
   let outputHTML = '';
-  outputHTML += `<source media="(min-width: 500px)" srcset="${IMAGE_BASE_URL}/w185${movie.poster_path} 1x, ${IMAGE_BASE_URL}/w500${movie.poster_path} 2x, ${IMAGE_BASE_URL}/w780${movie.poster_path} 3x, ${IMAGE_BASE_URL}/original${movie.poster_path}"></source>`;
-  outputHTML += `<source media="(min-width: 400px)" srcset="${IMAGE_BASE_URL}/w780${movie.backdrop_path} 1x, ${IMAGE_BASE_URL}/w1280${movie.backdrop_path} 2x, ${IMAGE_BASE_URL}/w1280${movie.backdrop_path} 3x, ${IMAGE_BASE_URL}/original${movie.backdrop_path}"></source>`;
-  outputHTML += `<source media="(min-width: 300px)" srcset="${IMAGE_BASE_URL}/w300${movie.backdrop_path} 1x, ${IMAGE_BASE_URL}/w780${movie.backdrop_path} 2x, ${IMAGE_BASE_URL}/w1280${movie.backdrop_path} 3x, ${IMAGE_BASE_URL}/original${movie.backdrop_path}"></source>`;
-  // Will add more media query later when actually working on the reponsive part. Right now it simply just load the poster when the screen width is above 500px.
+  outputHTML += `<source media="(min-width: 500px)" srcset="${IMAGE_BASE_URL}/w185${movie.poster_path} 1x, ${IMAGE_BASE_URL}/w500${movie.poster_path} 2x, ${IMAGE_BASE_URL}/w780${movie.poster_path} 3x, ${IMAGE_BASE_URL}/original${movie.poster_path}"></source>`; // eslint-disable-line max-len
+  outputHTML += `<source media="(min-width: 400px)" srcset="${IMAGE_BASE_URL}/w780${movie.backdrop_path} 1x, ${IMAGE_BASE_URL}/w1280${movie.backdrop_path} 2x, ${IMAGE_BASE_URL}/w1280${movie.backdrop_path} 3x, ${IMAGE_BASE_URL}/original${movie.backdrop_path}"></source>`; // eslint-disable-line max-len
+  outputHTML += `<source media="(min-width: 300px)" srcset="${IMAGE_BASE_URL}/w300${movie.backdrop_path} 1x, ${IMAGE_BASE_URL}/w780${movie.backdrop_path} 2x, ${IMAGE_BASE_URL}/w1280${movie.backdrop_path} 3x, ${IMAGE_BASE_URL}/original${movie.backdrop_path}"></source>`; // eslint-disable-line max-len
+  // Will add more media query later when actually working on the reponsive part.
+  // Right now it simply just load the poster when the screen width is above 500px.
   outputHTML += `<img src="${IMAGE_BASE_URL}/w300${movie.backdrop_path}"></img>`;
   picture.innerHTML = outputHTML;
 }
 
-getMovieById('420818'); // For testing, will remove later
+// For testing, will remove later
+const response = getMovieById('420818');
+
+response
+  .then(asJSON)
+  .then(updateUI)
+  .catch((err) => {
+    console.log(err); // eslint-disable-line no-console
+  });
